1606ebc5108edb40222d364009c1f28560543e14
Jyothisaika <96777274+Jyothisaika@users.noreply.github.com>
2022-05-26 17:24:34 +0530

    Add files via upload

---
+++ gamepage.js
@@ -0,0 +1,32 @@
+  // random value generated
+var y = Math.floor(Math.random() * 10 +1);
+
+var guess = 1;
+// number guessed by user
+function submit(){
+    
+    
+    var x = document.getElementById("guessField").value;
+
+    if(x == y)
+    {
+        alert("CONGRTULATIONS!!!"+playername+" YOU GUESSED IT RIGHT IN " + guess + " GUESS ");
+          guess= 1;      
+    }
+    else if(x > y) /* if guessed number is greater
+                   than actual number*/
+    {
+        guess++;
+        alert("OOPS SORRY!! TRY A SMALLER NUMBER");
+    }
+    else
+    {
+        guess++;
+        alert("OOPS SORRY!! TRY A GREATER  NUMBER");
+    }
+
+}
+
+function playAgain(){
+    y = Math.floor(Math.random() * 10 + 1);
+}  
